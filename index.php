01e1d49f5d39f83e813ab3920aab25e93598c902
Welpop <Welpop@hotmail.fr>
2016-06-30 12:09:41 +0200

    commit 3

--- index.php
+++ index.php
@@ -8,9 +8,22 @@ var_dump(fread($file, filesize("text.txt")));
 // Je ferme le fichier
 fclose($file);
 
+
+
 //popur écrire dans un fichier
 $file =fopen('texte.log',"a");
 fwrite($file, date("Y-m-d H:i:s").": le script a été ouvert \r \n");
 fclose($file);
+
+//fonctions équivalentes pour lire un fichier
+echo file_get_contents("texte.log");
+//équivalence pour écrire dans un fichier
+$content= file_get_contents("texte.log");
+$content.=date('Y-m-d H:i:s')." : FPC le script a été ouvert\r \n";
+file_put_contents("texte.log", $content);
+
+/*//supression de fichier
+unlink("texte.log");
+*/
 
 ?>
