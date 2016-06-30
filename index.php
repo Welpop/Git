8878952e6ee5a55a5af75abb9513c44a72627216
Welpop <Welpop@hotmail.fr>
2016-06-30 11:44:13 +0200

    commit 2

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php 
 	
-	
+
 // Pour lire un fichier avec fopen, j'ouvre le fichier
-$file = fopen("texte.txt", "r");
+$file = fopen("text.txt", "r");
 // Avec fread, je peux lire le fichier, je dois lui donner une taille
-var_dump(fread($file, filesize("texte.txt")));
+var_dump(fread($file, filesize("text.txt")));
 // Je ferme le fichier
 fclose($file);
 
-
+//popur écrire dans un fichier
+$file =fopen('texte.log',"a");
+fwrite($file, date("Y-m-d H:i:s").": le script a été ouvert \r \n");
+fclose($file);
 
 ?>
